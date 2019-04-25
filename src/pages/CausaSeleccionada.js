1fa63cd6a45cea84ca497687956c4786b3580604
Sebastian Crossa <crossasebastian@gmail.com>
2019-04-25 00:02:19 -0500

    Completed both left and right columns from the cause screen. Started upadtes section

--- src/pages/CausaSeleccionada.js
+++ src/pages/CausaSeleccionada.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import './CausaSeleccionada.css';
 import organizerImage from '../imgs/hcl-logo.jpg';
 
+import causeImage from '../imgs/DEFORESTACION_1.jpg';
+
 // Component imports
 import Navbar from '../layout/Navbar';
 import { Line } from 'rc-progress';
@@ -16,7 +18,63 @@ class CausaSeleccionada extends Component {
         <div className='container'>
           <div className='causaSeleccionada-columnas'>
             <div className='causaSeleccionada-columna-izquierda'>
-              <h1>izq</h1>
+              <div className='causaSeleccionada-columna-izquierda-container'>
+                <img
+                  src={causeImage}
+                  className='causaSeleccionada-columna-izquierda-galeria'
+                  alt='Cause image gallery'
+                />
+
+                <div className='causaSeleccionada-columna-izquierda-texto'>
+                  <div className='causaSeleccionada-columna-izquierda-desc causaSeleccionada-columna-izquierda-texto-especifico'>
+                    <div className='causaSeleccionada-columna-izquierda-desc-titulo causaSeleccionada-columna-izquierda-texto-titulo'>
+                      Descripcion
+                    </div>
+                    <div className='causaSeleccionada-columna-izquierda-desc-texto causaSeleccionada-columna-izquierda-texto-descripcion'>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                      Morbi aliquam leo eget nisi tempor, dictum venenatis diam
+                      finibus. Maecenas rhoncus mollis commodo. Proin eleifend
+                      lacus ac lectus faucibus, non commodo ante tempor. Proin
+                      blandit erat ut nibh aliquam consequat. Mauris mollis id
+                      ipsum ut luctus. Duis porttitor cursus enim, a semper
+                      sapien viverra non. Lorem ipsum dolor sit amet,
+                      consectetur adipiscing elit.
+                    </div>
+                  </div>
+
+                  <div className='causaSeleccionada-columna-izquierda-p1 causaSeleccionada-columna-izquierda-texto-especifico'>
+                    <div className='causaSeleccionada-columna-izquierda-p1-titulo causaSeleccionada-columna-izquierda-texto-titulo'>
+                      ¿Cómo puedo participar?
+                    </div>
+                    <div className='causaSeleccionada-columna-izquierda-p1-texto causaSeleccionada-columna-izquierda-texto-descripcion'>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                      Morbi aliquam leo eget nisi tempor, dictum venenatis diam
+                      finibus. Maecenas rhoncus mollis commodo. Proin eleifend
+                      lacus ac lectus faucibus, non commodo ante tempor. Proin
+                      blandit erat ut nibh aliquam consequat. Mauris mollis id
+                      ipsum ut luctus. Duis porttitor cursus enim, a semper
+                      sapien viverra non. Lorem ipsum dolor sit amet,
+                      consectetur adipiscing elit.
+                    </div>
+                  </div>
+
+                  <div className='causaSeleccionada-columna-izquierda-p2 causaSeleccionada-columna-izquierda-texto-especifico'>
+                    <div className='causaSeleccionada-columna-izquierda-p2-titulo causaSeleccionada-columna-izquierda-texto-titulo'>
+                      Actividades
+                    </div>
+                    <div className='causaSeleccionada-columna-izquierda-p2-texto causaSeleccionada-columna-izquierda-texto-descripcion'>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                      Morbi aliquam leo eget nisi tempor, dictum venenatis diam
+                      finibus. Maecenas rhoncus mollis commodo. Proin eleifend
+                      lacus ac lectus faucibus, non commodo ante tempor. Proin
+                      blandit erat ut nibh aliquam consequat. Mauris mollis id
+                      ipsum ut luctus. Duis porttitor cursus enim, a semper
+                      sapien viverra non. Lorem ipsum dolor sit amet,
+                      consectetur adipiscing elit.
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
 
             <div className='causaSeleccionada-columna-derecha'>
@@ -56,6 +114,12 @@ class CausaSeleccionada extends Component {
                 </div>
               </div>
             </div>
+          </div>
+
+          <div className='causaSeleccionada-actualizaciones'>
+            <p className='causaSeleccionada-actualizaciones-titulo'>
+              Actualizaciónes
+            </p>
           </div>
         </div>
       </div>
