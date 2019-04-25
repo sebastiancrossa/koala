4be0083cbc53d89ce62d084c49a57caabe8ca3e2
Sebastian Crossa <crossasebastian@gmail.com>
2019-04-24 21:30:59 -0500

    Linked causes to another page

---
+++ src/pages/CausaSeleccionada.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class CausaSeleccionada extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Causa seleccionada</h1>
+        <p>Informacion especifica</p>
+      </div>
+    );
+  }
+}
+
+export default CausaSeleccionada;
